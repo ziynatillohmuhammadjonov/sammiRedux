4e16738282486bea49c2e0f7576816a7d97a6266
Ziynatilloh <zmferpi2@gmail.com>
2023-11-07 09:53:46 +0500

    Setting configuration tailwindCss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navbar, Register, Login, Main } from "./components/";
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
